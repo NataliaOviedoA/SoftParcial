da30e35af597b158d58a673f55c66f2c63ad266c
NataliaOviedoA <oviedoacostanatalia@gmail.com>
2019-06-19 23:26:10 -0400

    nuevos servicios

--- src/environments/contanst.ts
+++ src/environments/contanst.ts
@@ -4,10 +4,10 @@ export class Constants {
   // =========================== Modificar esta IP ===========================
   public static SERVER_URL = 'http://localhost:8000';
   // =========================== Modificar esta IP ===========================
-  public static ENDPOINT_PACIENTES = Constants.SERVER_URL +  '/topicos/pacientes/';
-  public static ENDPOINT_DOCTOR = Constants.SERVER_URL +  '/topicos/doctores/';
-  public static ENDPOINT_TURNO = Constants.SERVER_URL +  '/topicos/turnos/';
-  public static ENDPOINT_ESPECIALIDAD = Constants.SERVER_URL +  '/topicos/especialidades/';
-  public static ENDPOINT_CONSULTA = Constants.SERVER_URL +  '/topicos/consultas/';
+  public static ENDPOINT_PACIENTES = Constants.SERVER_URL +  '/datos/paciente.php';
+  public static ENDPOINT_DOCTOR = Constants.SERVER_URL +  '/datos/doctor.php';
+  public static ENDPOINT_TURNO = Constants.SERVER_URL +  '/datos/turno.php';
+  public static ENDPOINT_ESPECIALIDAD = Constants.SERVER_URL +  '/datos/especialidad.php';
+  public static ENDPOINT_CONSULTA = Constants.SERVER_URL +  '/datos/consulta.php';
 
 }
