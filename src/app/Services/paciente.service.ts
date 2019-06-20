b6d9e8fd1e35e509184d6ac8328c88cff043b3f9
NataliaOviedoA <oviedoacostanatalia@gmail.com>
2019-06-20 02:04:19 -0400

    Servicio funcional

--- src/app/Services/paciente.service.ts
+++ src/app/Services/paciente.service.ts
@@ -23,9 +23,11 @@ export class PacienteService {
   addPaciente(form): Observable<Paciente[]> {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     console.log(form);
-  // this.http.post(Constants.ENDPOINT_PACIENTES_INSERTAR, form.value)
-  // .subscribe( data  => {console.log('POST Request is successful ', data); },
-  // error  => { console.log('Error', error); });
+  this.http.post(
+  Constants.ENDPOINT_PACIENTES_INSERTAR + '?ci=' + form.ci + '&nombre=' + form.nombre + '&sexo=' + form.sexo + '&fechaNacimiento=' + form.fechaNacimiento,
+  {headers : headers})
+  .subscribe( data  => {console.log('POST Request is successful ', data); },
+  error  => { console.log('Error', error); });
   return this.http.get<Paciente[]>(
     Constants.ENDPOINT_PACIENTES, { headers : headers }
   );
